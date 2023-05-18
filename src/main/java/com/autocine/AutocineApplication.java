f9424c12becb071418033c8e33b994ff128ac263
g-susvs <jesus.savs19@gmail.com>
2023-05-18 15:08:24 -0500

    Primer commit

---
+++ src/main/java/com/autocine/AutocineApplication.java
@@ -0,0 +1,12 @@
+package com.autocine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutocineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutocineApplication.class, args);
+	}
+}
---
+++ src/main/java/com/autocine/config/ConfigurationBean.java
@@ -0,0 +1,8 @@
+package com.autocine.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfigurationBean {
+
+}
